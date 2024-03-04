2a3638cf130d82d5ee0c9f9fe026d9d2560abe91
Michael <enjoyweaver@gmail.com>
2024-03-03 18:33:15 -0600

    !

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,8 @@ import { createClient } from "@supabase/supabase-js";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_URL!,
+  process.env.REACT_APP_SUPABASE_ANON_KEY!
 );
 
 const root = ReactDOM.createRoot(
